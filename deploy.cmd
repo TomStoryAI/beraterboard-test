60fdafcda2b313d57e0bf257f4af209649096d56
Your Name <not@viadee.de>
2019-04-15 19:05:07 +0200

    Kudu Updates

--- deploy.cmd
+++ deploy.cmd
@@ -106,12 +106,12 @@ IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
 )
 
 :: 4. Angular Prod Build
-IF EXIST "%DEPLOYMENT_SOURCE%/.angular-cli.json" (
-  echo Building App in %DEPLOYMENT_SOURCE%…
+IF EXIST "%DEPLOYMENT_SOURCE%\.angular.json" (
+  echo Building App in %DEPLOYMENT_SOURCE%
   pushd "%DEPLOYMENT_SOURCE%"
   :: call :ExecuteCmd !NPM_CMD! run build
   :: If the above command fails comment above and uncomment below one
-  call ./node_modules/.bin/ng build –prod
+  call ./node_modules/.bin/ng build --prod
   IF !ERRORLEVEL! NEQ 0 goto error
   popd
 )
