38f8dcd434419dc665459bf181c90a1b0377ce33
Your Name <not@viadee.de>
2019-04-15 20:19:48 +0200

    Kudu Updates

--- deploy.cmd
+++ deploy.cmd
@@ -88,12 +88,6 @@ goto :EOF
 :Deployment
 echo Handling node.js deployment.
 
-:: 1. KuduSync
-IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
-  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
-  IF !ERRORLEVEL! NEQ 0 goto error
-)
-
 :: 2. Select node version
 call :SelectNodeVersion
 
@@ -114,14 +108,20 @@ IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
 )
 
 :: 4. Angular Prod Build
-IF EXIST "%DEPLOYMENT_SOURCE%/.angular-cli.json" (
+IF EXIST "%DEPLOYMENT_SOURCE%\angular-cli.json" (
   echo Building App in %DEPLOYMENT_SOURCE%
   pushd "%DEPLOYMENT_SOURCE%"
   :: call :ExecuteCmd !NPM_CMD! run build
   :: If the above command fails comment above and uncomment below one
-  call ./node_modules/.bin/ng build --prod
+  call node_modules/.bin/ng build --prod
   IF !ERRORLEVEL! NEQ 0 goto error
   popd
+)
+
+:: 1. KuduSync
+IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
+  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
+  IF !ERRORLEVEL! NEQ 0 goto error
 )
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
